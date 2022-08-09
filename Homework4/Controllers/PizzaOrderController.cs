321b80106330652ad5782a1507817dffc9ae5130
artem.meniak@gmail.com <user@address.com>
2022-08-09 21:19:19 +0300

    Add and Get orders functionality added.

---
+++ Homework4/Controllers/PizzaOrderController.cs
@@ -0,0 +1,54 @@
+﻿using Homework4.Data;
+using Homework4.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework4.Controllers
+{
+    public class PizzaOrderController : Controller
+    {
+        private readonly PizzaDeliveryDbContext _pizzaDeliveryDbContext;
+
+        public PizzaOrderController(PizzaDeliveryDbContext pizzaDeliveryDbContext)
+        {
+            _pizzaDeliveryDbContext = pizzaDeliveryDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _pizzaDeliveryDbContext.Orders.ToListAsync();
+            return View(orders);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Order placeOrderRequest)
+        {
+            
+            var order = new Order()
+            {
+                CustomerName = placeOrderRequest.CustomerName,
+                Address = placeOrderRequest.Address,
+                Pizza = placeOrderRequest.Pizza,
+                Phone = placeOrderRequest.Phone,
+                Email = placeOrderRequest.Email,
+                AdditionalOrderInfo = placeOrderRequest.AdditionalOrderInfo
+            };
+
+            if (ModelState.IsValid)
+            {
+                await _pizzaDeliveryDbContext.Orders.AddAsync(order);
+                await _pizzaDeliveryDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Homework4/Models/Domain/Order.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Homework4.Data;
+using Homework4.Models.Base;
+using Homework4.Models.Enums;
+
+namespace Homework4.Models.Domain
+{
+    public class Order : BaseItem
+    {
+        [Required(ErrorMessage = "Wrong customer name!")]
+        [DisplayName("Customer Name")]
+        [StringLength(50)]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Wrong address!")]
+        [DisplayName("Address")]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "You must choose one pizza type for the order!")]
+        public PizzaEnum Pizza { get; set; }
+
+        [Required(ErrorMessage = "Wrong phone number!")]
+        [Phone]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public string? AdditionalOrderInfo { get; set; }
+    }
+}
